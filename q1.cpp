ca34a6e3f10aacb1581880abf2858f1e94d3d036
AngelAlducin <aualducin@gamil.com>
2022-10-17 09:34:36 -0700

    Done

--- q1.cpp
+++ q1.cpp
@@ -6,8 +6,8 @@ using namespace std;
 // Declare Function Prototypes
 //********************
 
-void getinput(int &n1, int &n2);
-void getinput(float &f1, float &f2);
+void getintput(int &n1, int &n2);
+void getintput(float &f1, float &f2);
 void swapTwoValues(int &n1, int &n2);
 void swapTwoValues(float &f1, float &f2);
 
@@ -15,13 +15,13 @@ int main()
 {
 	int num1, num2;
 	float fnum1, fnum2;
-	getinput(num1, num2);
+	getintput(num1, num2);
 	cout << " Before Swap" << setw(5) << num1 << setw(5) << num2 << setw(5) << endl;
 	swapTwoValues(num1, num2);
 	cout << " After Swap" << setw(5) << num1 << setw(5) << num2 << setw(5) << endl;
 
 	// -- Call overloaded function with float parameters
-	getinput(fnum1, fnum2);
+	getintput(fnum1, fnum2);
 	cout << setw(5) << fnum1 << setw(5) << fnum2 << setw(5) << endl;
 	swapTwoValues(fnum1, fnum2);
 	cout << setw(5) << fnum1 << setw(5) << fnum2 << setw(5) << endl;
@@ -37,7 +37,7 @@ void getintput(int &n1, int &n2)
 	cin >> n1 >> n2;
 }
 
-void getinput(float &f1, float &f2)
+void getintput(float &f1, float &f2)
 {
 	cout << "Enter two float numbers: ";
 	cin >> f1 >> f2;
